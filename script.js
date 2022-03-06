ace2123160f6e33b58fd29b26ce5d0fdb1b94dc3
Your Name <rajpnikoo@gamil.com>
2022-03-06 20:32:55 +0530

    mycv

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if (this.scrolly > 20);{
+            $(".nav") .addClass ('sticky');
+        }
+        
+           
+    }); 
+
+
+var typed = new Typed(".typing",{
+    strings:["Developer" ,"Coder" ,"Tester","Freelancer","Teko-Holik" ],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+});
+var typed = new Typed(".typing1",{
+    strings:["Teko-Holik" ,"Coder","Tester" ,"Freelancer","Developer" ],
+    typeSpeed:100,
+    backSpeed:80,
+    loop:true
+});
+}); 
